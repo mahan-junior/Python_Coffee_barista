a6b3c7bc8ec7e344fbe8578bfcd0ad855f6282aa
mahangordi <mahangordi97@gmail.com>
2024-05-02 00:37:45 +0400

    mahan_ptyhon

---
+++ #print('bro').py
@@ -0,0 +1,38 @@
+#print('bro')
+
+#print('Hello world')
+#print("Hello world")
+
+#print("""Hello sherak 
+#im your friend 
+#ok you are crazy""")
+#practice python !
+
+
+
+#print("I am Iron man \n" + "I am nigaa " + "I am popy" * 10#0)
+#print("Hello ewolrklfsdf")           
+#input("Whats is your name?")
+#print("Hello.............,thank you so much to coming")
+
+print("Welcome to my coffee shop !!!!!")
+name = input("What's your name? ")# the information...
+
+print("Hello " + name + " thank for order.")#first part 
+
+menu = "[ Black coffee , Wight coffee , Espersso , Latte ]: "
+name2 = input("what do you want to order ? " + menu )
+
+order = input()
+
+price = 8
+
+quantity = input("How many coffees would you like?\n ")
+
+total = price * int(quantity) #integer
+
+print("Thank you . your total is: $" + str(total))
+
+print("ok your " + name2 + "" + order+ " ready for you in a moment.")
+
+
